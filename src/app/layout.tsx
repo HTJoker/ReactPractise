adc2a00606a28594e38ef476cb17948ff7825d09
HTJoker <hersh.thakur@gmail.com>
2024-05-08 09:12:22 +0100

    converted TopNav function to arrow function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,13 +13,14 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-function TopNav() {
+const TopNav = () => {
   return (
     <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
       <div>Gallery</div>
+      <div>Sign In</div>
     </nav>
   );
-}
+};
 
 export default function RootLayout({
   children,
@@ -28,7 +29,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
+      <body
+        className={`font-sans ${inter.variable} flex flex-col gap-4 bg-emerald-700`}
+      >
         <TopNav />
         {children}
       </body>
