923b586da6837a001c6a15f8ba4aa65bd6ff7237
HTJoker <hersh.thakur@gmail.com>
2024-05-09 08:32:09 +0100

    added new post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,9 @@ export default async function HomePage() {
   return (
     <main>
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
